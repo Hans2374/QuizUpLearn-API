1279174832614a3ad0ee15a5a1b03b6467b6d14d
Hanz Nguyen <minhkhoi23704@gmail.com>
2026-02-27 21:57:16 +0700

    feat: Establish initial API infrastructure with Dockerization, JWT authentication, SignalR, Redis, and Firebase integration.

--- QuizUpLearn/QuizUpLearn.API/Program.cs
+++ QuizUpLearn/QuizUpLearn.API/Program.cs
@@ -109,13 +109,8 @@ if (FirebaseApp.DefaultInstance == null)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowFrontend");
 
